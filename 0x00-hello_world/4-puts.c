0d87e014a840334680cd3d9129ba0e9b1d55d224
lesliemakonjo <makonjoleslie@gmail.com>
2022-09-08 14:21:28 -0700

    Task 4 completed

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,22 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ *
+ *  * Simple program that uses print function
+ *
+ *   *
+ *
+ *    * Return: a string
+ *
+ *     */
+
+int main(void)
+
+{
+
+	    puts("\"Programming is like building a multilingual puzzle");
+
+	        return (0);
+
+}
