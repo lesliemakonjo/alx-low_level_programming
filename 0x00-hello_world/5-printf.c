aed48ca036dcb8c34753cb217d473cd9f343bdc0
lesliemakonjo <makonjoleslie@gmail.com>
2022-09-08 15:43:11 -0700

    done

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
+ *
 * main -entry point of the code
 * this program write a C program that prints exactly wth proper grammer , but the outcome is a piece of art
 * Return 0
 */
 int main(void)
 {
-	printf("with proper grammar , but the outcome is a piece of art ,  \n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return(0);
 }
