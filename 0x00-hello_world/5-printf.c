9c12ca3e3a48eab3aaa99c5872cbc09cc2ce8673
lesliemakonjo <makonjoleslie@gmail.com>
2022-09-08 15:27:16 -0700

    done

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+* main -entry point of the code
+* this program write a C program that prints exactly wth proper grammer , but the outcome is a piece of art
+* Return 0
+*/
+int main(void)
+{
+   perfect(*with prope grammar , but the outcome is a piece of art ,  \n*);
